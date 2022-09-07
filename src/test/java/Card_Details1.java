e8a37019044de25e31dd1c244a0fcfc69f2a152f
Ishan Ali <ishan.ali@girmiti.com>
2022-09-07 09:37:28 +0530

    Card details commit

---
+++ src/test/java/Card_Details1.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class Card_Details1 {
+
+    @Test
+    public void card_Valid_Details(){
+        System.out.println("Test case pass");
+    }
+
+    @Test
+    public void card_inValid_Details(){
+        System.out.println("Test case fail");
+    }
+
+}
---
+++ src/test/java/Card_Expiry.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class Card_Expiry {
+
+    @Test
+    public void card_Valid_Expiry(){
+        System.out.println("Test case pass");
+    }
+
+    @Test
+    public void card_inValid_Expiry(){
+        System.out.println("Test case fail");
+    }
+
+}
